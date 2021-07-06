90456e994bac71f9039af6ce20986d569de29ce0
chrisnguyen127 <42982458+ThatOneKidChris@users.noreply.github.com>
2021-07-06 23:49:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+import os
+import requests
+import json
+import random
+from replit import db
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if msg.startswith("$favorite"):
+    starters = ["bulbasaur", "squirtle", "charmander"]
+    favorite = random.choice(starters)
+    await message.channel.send("I kinda like {}...".format(favorite))
+
+  if msg.startswith('$hey'):
+    await message.channel.send("Hey, {}! You come here often?".format(message.author.mention))
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
