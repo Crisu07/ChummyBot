be67a7eae51ac704712c74e6a49c137d5d676af1
Mariel Urbano <imarillurbano@gmail.com>
2021-08-18 00:27:20 +0000

    Changed function name to be consistent with the rest

--- checkCurse.py
+++ checkCurse.py
@@ -1,5 +1,5 @@
 #checks for exact matches of swear/bad words from someone's sentence and our dictionary 
-def curseCheck( curse, line):
+def check_curse( curse, line):
   for i in line:
     if i in curse:
       return True
